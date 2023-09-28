256cc8d24c94db4afd430e9fe71a944744b18568
gabriel-nigro <74302169+gabriel-nigro@users.noreply.github.com>
2023-09-28 14:21:50 +0000

    feat(review): add elfsight-widget google review

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Features from "./Features/index.js";
 import Navbar from "./Header/index.js";
 import Home from "./Home/index.js";
+import Review from "./Review/index.js";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       </div>
       <div className="py-10">
         <Features />
+      </div>
+      <div className="py-10">
+        <Review />
       </div>
     </div>
   );
---
+++ src/Review/index.js
@@ -0,0 +1,11 @@
+import { ElfsightWidget } from 'react-elfsight-widget';
+
+const Review = () => {
+    return (
+        <div className="container" id="reviews">
+            <ElfsightWidget widgetId="e4dc8996-3035-4064-9d85-9c2e32860882" lazy />
+        </div>
+    );
+  };
+  
+  export default Review;
