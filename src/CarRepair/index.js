d873ae4baf95724a6debcbddfd4ae7548ec80089
gabriel-nigro <74302169+gabriel-nigro@users.noreply.github.com>
2023-10-04 20:57:52 +0000

    feat: fix background collor and font size

--- src/CarRepair/index.js
+++ src/CarRepair/index.js
@@ -7,9 +7,9 @@ const CarRepair = () => {
         <div className="row d-flex justify-content-center align-items-center">
             <div className="col-6"><img className="rounded img-fluid" src={carRepair} alt="car repair"/></div>
             <div className="col-6 d-flex flex-column justify-content-center">
-                <p className="fs-5">About CarRepair</p>
+                <p className="fs-4">About CarRepair</p>
                 <p className="carRepair-title fw-bold">We Make Your Car Comfortable</p>
-                <p className="fs-6">CarRepair is one of the workshops that can serve home service calls. All mechanics at CarRepair already have a long experience, so high quality mechanics to solve all your car problems.</p>
+                <p className="fs-6-car-repair">CarRepair is one of the workshops that can serve home service calls. All mechanics at CarRepair already have a long experience, so high quality mechanics to solve all your car problems.</p>
             </div>
         </div>
     </div>
