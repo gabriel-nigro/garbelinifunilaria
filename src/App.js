31bd47f100af459f5f0cf095e86c701998773cc9
joaopedrobello <joaopedrogbello@gmail.com>
2023-10-04 17:23:19 -0300

    feat: iniciando a feature about car repair

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Features from "./Features/index.js";
 import Navbar from "./Header/index.js";
 import Home from "./Home/index.js";
+import CarRepair from "./CarRepair/index.js";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <Navbar />
       <div className="py-10">
         <Home />
+      </div>
+      <div className="py-10 carRepair">
+        <CarRepair />
       </div>
       <div className="py-10">
         <Features />
---
+++ src/CarRepair/index.js
@@ -0,0 +1,19 @@
+import "./CarRepair.css";
+import carRepair from "../assets/carRepair.png";
+
+const CarRepair = () => {
+  return (
+    <div className="container">
+        <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-6"><img className="rounded img-fluid" src={carRepair} alt="car repair"/></div>
+            <div className="col-6 d-flex flex-column justify-content-center">
+                <p className="fs-5">About CarRepair</p>
+                <p className="carRepair-title fw-bold">We Make Your Car Comfortable</p>
+                <p className="fs-6">CarRepair is one of the workshops that can serve home service calls. All mechanics at CarRepair already have a long experience, so high quality mechanics to solve all your car problems.</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default CarRepair;
