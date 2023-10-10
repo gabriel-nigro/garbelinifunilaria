aef0607e73376e2f43ade70dbb1dcbd20dee9260
joaopedrobello <joaopedrogbello@gmail.com>
2023-10-10 12:25:35 -0300

    feat: added foooter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Features from "./Features/index.js";
 import Navbar from "./Header/index.js";
 import Home from "./Home/index.js";
 import CarRepair from "./CarRepair/index.js";
+import Footer from "./Footer/index.js"
 import "./App.css";
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
       </div>
       <div className="py-10">
         <Features />
+      </div>
+      <div className="py-10 footer">
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Footer/map.js
@@ -0,0 +1,30 @@
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: -23.576739935745856,
+      lng: -46.58407639632665,
+    },
+    zoom: 17
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '20vh', width: '80%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
