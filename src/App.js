57cb7d8e3944686409d5387fb38cc585f5ab6eba
Gabriel Nigro <odp110358@odontoprev.com.br>
2023-10-18 12:44:07 -0300

    refactor(footer): inserted api key enviroment var and added marker

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Features from "./Features/index.js";
 import Navbar from "./Header/index.js";
 import Home from "./Home/index.js";
 import CarRepair from "./CarRepair/index.js";
-import Footer from "./Footer/index.js"
+import Footer from "./Footer/index.js";
 import "./App.css";
-import Services from "./Services/index.js"
+import Services from "./Services/index.js";
 
 function App() {
   return (
@@ -19,10 +19,11 @@ function App() {
       <div className="py-10">
         <Features />
       </div>
-      <div className="py-10 footer">
-        <Footer />
       <div className="py-10">
         <Services />
+      </div>
+      <div className="py-10 footer">
+        <Footer />
       </div>
     </div>
   );
--- src/Footer/map.js
+++ src/Footer/map.js
@@ -1,30 +1,34 @@
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-export default function SimpleMap(){
-  const defaultProps = {
+export default function SimpleMap() {
+  const location = {
     center: {
-      lat: -23.576739935745856,
-      lng: -46.58407639632665,
+      lat: -23.57673454284668,
+      lng: -46.58407211303711,
     },
-    zoom: 17
+    zoom: 17,
+  };
+
+  // https://stackoverflow.com/questions/41405343/adding-marker-to-google-maps-in-google-map-react
+  const renderMarkers = (map, maps) => {
+    let marker = new maps.Marker({
+      position: location.center,
+      map,
+      title: "Garbelini Funilaria e Pintura",
+    });
+    return marker;
   };
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '25vh', width: '100%' }}>
+    <div style={{ height: "25vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          // text="My Marker"
-        />
-      </GoogleMapReact>
+        bootstrapURLKeys={{ key: process.env.API_KEY }}
+        defaultCenter={location.center}
+        defaultZoom={location.zoom}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+      />
     </div>
   );
 }
