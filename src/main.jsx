ac54682c8347f96b2273cacf66f4ad9706a4418a
Gabriel Nigro <odp110358@odontoprev.com.br>
2023-10-25 16:37:27 -0300

    chore: add google tag manager and google analytics 4

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import ReactGA from "react-ga4";
+import TagManager from "react-gtm-module";
+
+// Google Analytics and Tag Manager
+ReactGA.initialize("G-T2460GV2MY");
+TagManager.initialize({ gtmId: "GTM-KQR28PQC" });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
