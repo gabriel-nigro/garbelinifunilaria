279f70d53d8cf79470931d0a5308f3234e552491
Gabriel Nigro <odp110358@odontoprev.com.br>
2023-11-23 17:11:46 -0300

    chore: a bunch of small fixes

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,7 +1,14 @@
-const Service = ({ title, icon }) => {
+// Components
+import Picture from "../Picture/Picture.jsx";
+
+// Style
+import "./Service.css"
+
+
+const Service = ({ title, icon, iconFallback }) => {
   return (
     <div className="px-4-5 py-3">
-      <img src={icon} alt="Service Icon" />
+      <Picture src={icon} fallback={iconFallback} alt={title} className="service-icon" />
       <p className="fs-4 fw-bold">{title}</p>
     </div>
   );
